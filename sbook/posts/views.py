da5e54b8f74560b4823536625dfc2f67b75d2dbd
sudip6164 <sudippradhanadgj@gmail.com>
2025-02-06 08:18:29 +0545

    posts list

--- sbook/posts/views.py
+++ sbook/posts/views.py
@@ -4,6 +4,10 @@ from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
 @login_required
 def post_create(request):
     if request.method == 'POST':
@@ -12,7 +16,7 @@ def post_create(request):
             post = form.save(commit=False)
             post.user = request.user
             post.save()
-            return redirect('home')
+            return redirect('post_list')
     else:
         form = PostForm()
     return render(request, 'posts/post_form.html', {'form': form})
