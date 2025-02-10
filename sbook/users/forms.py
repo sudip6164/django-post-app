d44c4b0f1b5eca25f77553ec8ee8f6386a3f5fb3
sudip6164 <sudippradhanadgj@gmail.com>
2025-02-10 09:06:24 +0545

    signup. css

--- sbook/users/forms.py
+++ sbook/users/forms.py
@@ -1,5 +1,23 @@
 from django import forms
 from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    # Optionally add custom help_text
+    password1 = forms.CharField(
+        widget=forms.PasswordInput,
+        help_text="Your password must contain at least 8 characters, "
+                  "and can't be a commonly used password or entirely numeric."
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput,
+        help_text="Enter the same password as before for verification."
+    )
 
 class ProfileForm(forms.ModelForm):
     class Meta:
